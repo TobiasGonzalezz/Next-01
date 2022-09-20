cd53bfb005f564a25ea19450b2138c42bcc32f4f
Tobias Gonzalez <103076990+TobiasGonzalezz@users.noreply.github.com>
2022-09-20 19:42:09 -0300

    App de noticias argentinas fachera

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import PageLayout from '../components/PageLayout'
 import Link from 'next/link'
 
-export default function Home({articles}) {
+export default function Home({ articles }) {
   return (
     <PageLayout title='NewsApp - Home'>
       <div className={styles.container}>
@@ -11,7 +11,7 @@ export default function Home({articles}) {
         {articles.length > 0 && articles.map((articles, index) => (
           <div key={index}>
             <h2 className={styles.h2}>{articles.title}</h2>
-            <Image
+            {/* <Image
             className={styles.image}
             src={`/api/imageproxy?url=${encodeURIComponent(articles.urlToImage)}`}
             alt={`image for the articles ${articles.title}`} 
@@ -19,11 +19,20 @@ export default function Home({articles}) {
             height={'300px'}
             layout='responsive'
             property={index < 2}
-            />
+            /> */}
+            <img
+              className={styles.image}
+              src={`/api/imageproxy?url=${encodeURIComponent(articles.urlToImage)}`}
+              alt={`image for the articles ${articles.title}`}
+              width={'450px'}
+              height={'300px'}
+              layout='responsive'
+              property={index < 2} 
+              />
             <p>
               {articles.description} <Link href={articles.url}>seguir leyendo...</Link>
             </p>
-            
+
           </div>
         ))}
       </div>
@@ -35,7 +44,7 @@ export async function getStaticProps() {
   const response = await fetch('https://newsapi.org/v2/top-headlines?sources=google-news-ar&apiKey=7f9cc0212a914e08aeb001980d97b781')
   const { articles } = await response.json()
 
-  return{
+  return {
     props: {
       articles
     }
